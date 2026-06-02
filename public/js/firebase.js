7ea2737311274fce91fb7eb03ef854e6755e833c
srao2445-art <srao2445@gmail.com>
2026-06-02 14:06:58 +0530

    Rebuild Creator Studio as Firebase static app

---
+++ public/js/firebase.js
@@ -0,0 +1,8 @@
+import { initializeApp } from 'https://www.gstatic.com/firebasejs/10.12.5/firebase-app.js';
+import { getAuth } from 'https://www.gstatic.com/firebasejs/10.12.5/firebase-auth.js';
+import { getFirestore } from 'https://www.gstatic.com/firebasejs/10.12.5/firebase-firestore.js';
+import { firebaseConfig } from './firebase-config.js';
+
+const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
+export const db = getFirestore(app);
